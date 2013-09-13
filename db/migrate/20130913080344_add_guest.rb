5b7561c733a8037c988eb6faeaf3a01ed652c1be
ducluong <luongtranduc@gmail.com>
2013-09-13 15:09:56 +0700

    fix

--- db/migrate/20130913080344_add_guest.rb
+++ db/migrate/20130913080344_add_guest.rb
@@ -1,7 +1,7 @@
 class AddGuest < ActiveRecord::Migration
   def change
-    User.create({:name => "Guest", :email => "info@mysexdata.com", :facebook_id => "35353", :status => 1, :facebook_photo => "test.png", 
-              :profile_photo => "test.png", :age => 20, :birthday => "1993-20-10", :startday => "2013-10-09", :eye_color=>1, :hair_color => 1,
+    User.create({:user_id => 1, :name => "Guest", :email => "info@mysexdata.com", :facebook_id => "35353", :status => 1, :facebook_photo => "test.png", 
+              :profile_photo => "test.png", :age => 20, :birthday => "1993-10-10", :startday => "2013-10-09", :eye_color=>1, :hair_color => 1,
                :job => "it", :height => 1, :sex_interest => 0, :sex_gender => 1, :hairdressing => 0, :password => '1234', :password_confirmation => '1234',
                :preferences => ['test'], :premium => 1})
   end
